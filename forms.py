2bdf3aae8c094230186889ec344a1d76b2273207
Augustine Alul <117871941+21Alul21@users.noreply.github.com>
2024-07-28 03:32:36 +0100

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,14 +1,21 @@
-from flask_wtforms import FlaskForm
-from wtforms import StringField, SubmitField, SelectField
-from wtforms.validators import DataRequired
+from flask_wtf import   FlaskForm
+from wtforms import StringField, SubmitField, SelectField, IntegerField
+from wtforms.validators import DataRequired, NumberRange
 
 class PredictionForm(FlaskForm):
     """ class for the prediction form """
 
-    serviced = SelectField('serviced', validators=[DataRequired()])
-    newly_built = SelectField('newly built', validators=[DataRequired()]) 
-    furnished = SelectField('furnished', validators=[DataRequired()])
-    bedrooms = StringField('number of bedrooms', validators=[DataRequired()])
-    bathrooms = StringField('number of bathrooms', validators=[DataRequired()])
-    toilets = StringField('nuber of toilets', validators=[DataRequired()])
+    serviced = SelectField('serviced', validators=[DataRequired(message='this field is required')], choices=[('Yes'), ('No')])
+    newly_built = SelectField('newly built', validators=[DataRequired(message='this field is required')], choices=[('Yes'), ('No')]) 
+    furnished = SelectField('furnished', validators=[DataRequired(message='this field is required')], choices=[('Yes'), ('No')])
+    bedrooms = IntegerField('number of bedrooms', validators=[DataRequired(message='this field is required')])
+    bathrooms = IntegerField('number of bathrooms', validators=[DataRequired(message='this field is required')])
+    toilets = IntegerField('number of toilets', validators=[DataRequired(message='this field is required'), \
+                           NumberRange(min=0, message='please enter a valid number')])
+                                                           
     
+
+class UkPredictionForm(FlaskForm):
+    """ class for handling Uk prediction form """
+
+    bedrooms = IntegerField('Number of bedrooms', validators=[DataRequired(message='this field is required'), NumberRange(min=0, message='please enter a valid number')])
